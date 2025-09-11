93fe852b43e9dcc9f247a7d9c624856aa1060599
Leo Nicolle <leo@linkurio.us>
2025-09-11 15:40:59 +0200

    filter state on add too

--- packages/ogma-annotations/src/interaction/add.ts
+++ packages/ogma-annotations/src/interaction/add.ts
@@ -1,6 +1,6 @@
 import { Links } from "../links";
 import { Store } from "../store";
-import { Arrow } from "../types";
+import { Annotation, Arrow } from "../types";
 
 export class Add {
   private links: Links;
@@ -8,18 +8,16 @@ export class Add {
   constructor(store: Store, links: Links) {
     this.store = store;
     this.links = links;
-    this.store.subscribe(this.onAddArrow);
+    this.store.subscribe((state) => state.features, this.onAddArrow);
   }
 
   private onAddArrow = (
-    newState: ReturnType<Store["getState"]>,
-    prevState: ReturnType<Store["getState"]>
+    newFeatures: Record<string, Annotation>,
+    prevFeatures: Record<string, Annotation>
   ) => {
     const state = this.store.getState();
-    const oldIds = new Set(Object.keys(prevState.features));
-    const newIds = Object.keys(newState.features).filter(
-      (id) => !oldIds.has(id)
-    );
+    const oldIds = new Set(Object.keys(prevFeatures));
+    const newIds = Object.keys(newFeatures).filter((id) => !oldIds.has(id));
     newIds.forEach((id) => {
       const feature = state.getFeature(id);
       if (!feature || feature.properties.type !== "arrow") return;
