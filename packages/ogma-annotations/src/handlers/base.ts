1d790948cfff7a12806e0110ef9fd3ed714ba4fc
w8r <alex@milevski.co>
2025-09-16 11:43:05 +0200

    Forgot to add the file

---
+++ packages/ogma-annotations/src/handlers/base.ts
@@ -0,0 +1,125 @@
+import Ogma, { Point } from "@linkurious/ogma";
+import { Store } from "../store";
+import { Annotation, Cursor, Id } from "../types";
+import { clientToContainerPosition } from "../utils";
+
+export abstract class Handler<
+  T extends Annotation,
+  Handle
+> extends EventTarget {
+  protected annotation: Id | null = null;
+  protected ogma: Ogma;
+  protected dragging: boolean = false;
+  protected dragStartPoint?: Point;
+  protected hoveredHandle?: Handle;
+  protected ogmaPanningOption: boolean = false;
+  protected store: Store;
+
+  constructor(ogma: Ogma, store: Store) {
+    super();
+    this.store = store;
+    this.ogma = ogma;
+  }
+
+  handleMouseMove = (evt: MouseEvent): void => {
+    // compute the distance between the mouse and the edges of te box
+    if (!this.isActive()) return;
+    //const wasHovered = Boolean(this.hoveredHandle);
+    if (!this.dragging) this._detectHandle(evt, this.ogma.view.getZoom());
+    else if (this.dragStartPoint) this.onDrag(evt);
+  };
+
+  handleMouseDown = (evt: MouseEvent): void => {
+    if (!this.isActive() || this.dragging || !this.hoveredHandle) return;
+
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    // start resizing
+    this.dragging = true;
+    this.dragStartPoint = this.clientToCanvas(evt);
+    this.onDragStart(evt);
+    this.dispatchEvent(new Event("dragstart"));
+    this.ogmaPanningOption = Boolean(
+      this.ogma.getOptions().interactions?.pan?.enabled
+    );
+    this.ogma.setOptions({
+      interactions: { pan: { enabled: false } }
+    });
+  };
+
+  handleMouseUp = (evt: MouseEvent): void => {
+    if (!this.isActive() || !this.dragging) return;
+    this.dragging = false;
+    this.ogma.setOptions({
+      interactions: { pan: { enabled: this.ogmaPanningOption } }
+    });
+    this.onDragEnd(evt);
+    this.dispatchEvent(new Event("dragend"));
+  };
+
+  cancelEdit() {
+    if (!this.isActive() || this.annotation === null) return;
+    this.clearDragState();
+  }
+
+  // Keyboard events
+  handleKeyDown?(evt: KeyboardEvent): void;
+  handleKeyUp?(evt: KeyboardEvent): void;
+
+  protected clearDragState() {
+    this.dragging = false;
+    this.dragStartPoint = undefined;
+    this.hoveredHandle = undefined;
+    this.ogma.setOptions({
+      interactions: { pan: { enabled: this.ogmaPanningOption } }
+    });
+  }
+
+  protected commitChange() {
+    // Commit all live updates to create a single history entry
+    this.store.getState().commitLiveUpdates();
+  }
+
+  /**
+   * Detects which handle is being hovered over.
+   * @param evt Mouse event
+   */
+  protected abstract _detectHandle(evt: MouseEvent, zoom: number): void;
+  /**
+   * Handles the dragging of the selected handle.
+   * @param evt Mouse event
+   */
+  protected abstract onDrag(evt: MouseEvent): void;
+
+  protected abstract onDragStart(evt: MouseEvent): void;
+  protected abstract onDragEnd(evt: MouseEvent): void;
+
+  protected clientToCanvas(evt: MouseEvent): Point {
+    const ogma = this.ogma;
+    const screenPoint = clientToContainerPosition(evt, ogma.getContainer());
+    return ogma.view.screenToGraphCoordinates(screenPoint);
+  }
+
+  setAnnotation(annotation: T | null): void {
+    this.annotation = annotation ? annotation.id : null;
+  }
+
+  getAnnotation(): T | undefined {
+    return this.store.getState().getFeature(this.annotation!) as T;
+  }
+
+  protected setCursor(cursor: Cursor) {
+    const container = this.ogma.getContainer()?.firstChild;
+    if (container) (container as HTMLElement).style.cursor = cursor;
+  }
+
+  stopEditing() {
+    this.setAnnotation(null);
+    this.clearDragState();
+  }
+
+  isActive() {
+    return this.annotation !== null;
+  }
+}
