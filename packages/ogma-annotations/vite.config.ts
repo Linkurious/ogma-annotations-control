56474e50869e90f189c9bd5c52b2c43444e58b65
Alex Milevski <alex@milevski.co>
2025-11-19 09:29:40 +0100

    Cleanup

--- packages/ogma-annotations/vite.config.ts
+++ packages/ogma-annotations/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+
 import { resolve } from "path";
 import { name } from "./package.json";
 
@@ -8,24 +9,24 @@ export default defineConfig({
   plugins: [
     dts({
       outDir: "dist/types",
-      rollupTypes: true,
-    }),
+      rollupTypes: true
+    })
   ],
   build: {
     sourcemap: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       fileName: (format) => `index.${format === "umd" ? "" : "m"}js`,
-      name,
+      name
     },
     rollupOptions: {
       external: ["@linkurious/ogma"],
       output: {
         globals: {
-          "@linkurious/ogma": "Ogma",
-        },
-      },
+          "@linkurious/ogma": "Ogma"
+        }
+      }
     },
-    minify: true,
-  },
+    minify: true
+  }
 });
