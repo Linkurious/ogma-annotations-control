0d12908b0b57e6c156ca60c37616c17b30b72a6e
Leo Nicolle <leo@linkurio.us>
2025-11-20 16:06:17 +0100

    removed dummy test

--- packages/ogma-annotations/test/e2e/snapping.test.ts
+++ packages/ogma-annotations/test/e2e/snapping.test.ts
@@ -6,7 +6,7 @@ describe("Snapping", () => {
   let nodeBottom: { x: number; y: number };
 
   beforeAll(async () => {
-    await session.start();
+    await session.start(false);
   });
 
   afterAll(async () => {
@@ -48,6 +48,7 @@ describe("Snapping", () => {
         start.y,
         createArrow(start.x, start.y, start.x, start.y)
       );
+      console.log("ICI", editor.getAnnotations().features);
       return { topLeft, center, bottomRight, nodeBottom, start };
     });
     bottomRight = pts.bottomRight;
@@ -80,56 +81,4 @@ describe("Snapping", () => {
       })
     ).toBe(true);
   }, 5000);
-
-  it.each([0, 2])(
-    `should snap to a node when dragging the %i handle`,
-    async (handle) => {
-      // attach the arrow to the node
-      await session.page.mouse.move(nodeBottom.x, nodeBottom.y, { steps: 10 });
-      await session.page.mouse.up();
-      await session.page.mouse.down();
-      // move the arrow away from the node
-      await session.page.evaluate(
-        ({ handle }) => {
-          const arrow = editor.getAnnotations().features[0];
-          // @ts-expect-error private
-          editor.arrows.draggedHandle = handle;
-          // @ts-expect-error private
-          editor.arrows.startDragging(arrow, 0, 0);
-        },
-        { handle }
-      );
-      await session.page.mouse.move(200, 100, { steps: 20 });
-      await session.page.mouse.up();
-      // move the node
-      const nodePos = await session.page.evaluate(() => {
-        ogma.getNode("test")?.setSelected(true);
-        return ogma.getNode("test")!.getPositionOnScreen();
-      });
-      await session.page.mouse.move(nodePos.x, nodePos.y, { steps: 10 });
-      await session.page.mouse.down();
-      await session.page.mouse.move(bottomRight.x, bottomRight.y, {
-        steps: 10
-      });
-
-      // check that the arrow is not attached to the node
-      const { node, arrowPoints } = await session.page.evaluate(() => {
-        return {
-          node: ogma.getNode("test")?.getPosition(),
-          arrowPoints: editor.getAnnotations().features[0].geometry
-            .coordinates as number[][]
-        };
-      });
-      return expect(
-        arrowPoints.every(([x1, y1]) => {
-          const { x, y } = node!;
-          const dx = x1 - x;
-          const dy = y1 - y;
-          const dist = Math.sqrt(dx * dx + dy * dy);
-          return dist > 10;
-        })
-      ).toBe(true);
-    },
-    5000
-  );
 });
